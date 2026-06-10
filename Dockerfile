pogzie/scylla-openldap-demo

commit 96476fe4961bc66e4ab4c89698a71e8e449fc563

Update Dockerfile
Updating dockerfile to use latest version of Scylla as of this writing.

Dockerfile
@@ -1,5 +1,6 @@
 # The image to use as a base image
-FROM scylladb/scylla-enterprise:2024.1.14
+#FROM scylladb/scylla-enterprise:2024.1.14
+FROM scylladb/scylla:2026.1.3
 
 # Install system packages
 USER root

commit 0c09c00e1ccfd17e565f6b64ccb8ee0ed66b57a0

Fix compatibility with ScyllaDB 2026.1.3 (Rocky Linux 9)
- Replace apt with microdnf package manager for Rocky Linux 9
- Update package names: sasl2-bin→cyrus-sasl, vim→vim-minimal, openldap-utils→openldap-clients
- Convert saslauthd config from Debian format (/etc/default) to RPM format (/etc/sysconfig)
- Add supervisord integration for automatic saslauthd startup
- Update README with new commands (supervisorctl instead of service)
- Add FIXES_APPLIED.md documenting all changes

Fixes the build failure caused by commit 96476fe which updated the base
image from Debian-based scylla-enterprise:2024.1.14 to Rocky Linux-based
scylla:2026.1.3 without updating the package manager.

Dockerfile
@@ -4,5 +4,10 @@ FROM scylladb/scylla:2026.1.3
 
 # Install system packages
 USER root
-RUN apt update && apt install -y sasl2-bin vim openldap-utils
-COPY ./scylla/saslauthd /etc/default/saslauthd
+RUN microdnf install -y cyrus-sasl cyrus-sasl-plain \
+    vim-minimal openldap-clients && microdnf clean all
+
+# Configure saslauthd
+COPY ./scylla/saslauthd /etc/sysconfig/saslauthd
+COPY ./scylla/saslauthd-supervisor.conf /etc/supervisord.conf.d/saslauthd.conf
+RUN mkdir -p /var/run/saslauthd

scylla/saslauthd
@@ -1,60 +1,19 @@
 #
-# Settings for saslauthd daemon
-# Please read /usr/share/doc/sasl2-bin/README.Debian for details.
+# Settings for saslauthd daemon (RPM/Rocky Linux format)
+# This file is read by the saslauthd init script on RPM-based systems.
 #
 
-# Description of this saslauthd instance. Recommended.
-# (suggestion: SASL Authentication Daemon)
-DESC="SASL Authentication Daemon"
+# Directory for saslauthd's socket, pid file, and other runtime files.
+# This MUST be set for saslauthd to run properly.
+SOCKETDIR=/var/run/saslauthd
 
-# Short name of this saslauthd instance. Strongly recommended.
-# (suggestion: saslauthd)
-NAME="saslauthd"
+# Authentication mechanism to use.
+# Options: pam, ldap, kerberos5, rimap, shadow, sasldb, getpwent
+# When using ldap, configuration is read from /etc/saslauthd.conf
+MECH=ldap
 
-# Which authentication mechanisms should saslauthd use? (default: pam)
-#
-# Available options in this Debian package:
-# getpwent  -- use the getpwent() library function
-# kerberos5 -- use Kerberos 5
-# pam       -- use PAM
-# rimap     -- use a remote IMAP server
-# shadow    -- use the local shadow password file
-# sasldb    -- use the local sasldb database file
-# ldap      -- use LDAP (configuration is in /etc/saslauthd.conf)
-#
-# Only one option may be used at a time. See the saslauthd man page
-# for more information.
-#
-# Example: MECHANISMS="ldap"
-MECHANISMS="ldap"
-
-# Additional options for this mechanism. (default: none)
-# See the saslauthd man page for information about mech-specific options.
-MECH_OPTIONS=""
-
-# How many saslauthd processes should we run? (default: 5)
-# A value of 0 will fork a new process for each connection.
-THREADS=5
-
-# Other options (default: -c -m /var/run/saslauthd)
-# Note: You MUST specify the -m option or saslauthd won't run!
-#
-# WARNING: DO NOT SPECIFY THE -d OPTION.
-# The -d option will cause saslauthd to run in the foreground instead of as
-# a daemon. This will PREVENT YOUR SYSTEM FROM BOOTING PROPERLY. If you wish
-# to run saslauthd in debug mode, please run it by hand to be safe.
-#
-# See /usr/share/doc/sasl2-bin/README.Debian for Debian-specific information.
-# See the saslauthd man page and the output of 'saslauthd -h' for general
-# information about these options.
-#
-# Example for chroot Postfix users: "-c -m /var/spool/postfix/var/run/saslauthd"
-# Example for non-chroot Postfix users: "-c -m /var/run/saslauthd"
-#
-# To know if your Postfix is running chroot, check /etc/postfix/master.cf.
-# If it has the line "smtp inet n - y - - smtpd" or "smtp inet n - - - - smtpd"
-# then your Postfix is running in a chroot.
-# If it has the line "smtp inet n - n - - smtpd" then your Postfix is NOT
-# running in a chroot.
-OPTIONS="-V -c -m /var/run/saslauthd"
-START=yes
+# Additional flags for saslauthd.
+# -V: Enable verbose logging
+# -c: Enable credential caching
+# Note: Do NOT use -d (daemon/foreground mode) as it prevents proper startup
+FLAGS="-V -c"

scylla/saslauthd-supervisor.conf
@@ -1,0 +1,12 @@
+[program:saslauthd]
+command=/usr/sbin/saslauthd -a ldap -V -c -m /var/run/saslauthd -d
+directory=/var/run/saslauthd
+autostart=true
+autorestart=true
+startsecs=5
+stopwaitsecs=10
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+priority=10